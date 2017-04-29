83b06448a3f74d372a93f61b2602b73fc0c2d6e5
v.m.torres.cabrera <v.m.torres.cabrera@avanade.com>
2017-04-29 16:06:31 -0500

    font awesome fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomMaterialModule } from './angular-material.module';
 
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { BannerComponent } from './banner/banner.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,7 @@ import { BannerComponent } from './banner/banner.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule, NgbModule.forRoot()
+    HttpModule, NgbModule.forRoot(), CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
